9124154b13cd2b81856c22437c45d658f7f02a40
hyew0 <lhy3899@gmail.com>
2025-12-22 20:53:13 +0900

    설명 추가

--- src/B01_InputOutputAndArithmethic/B01_2557_HelloWorld.java
+++ src/B01_InputOutputAndArithmethic/B01_2557_HelloWorld.java
@@ -1,5 +1,18 @@
 package B01_InputOutputAndArithmethic;
 
+/*
+시간 제한	메모리 제한	제출	    정답	    맞힌 사람	정답 비율
+1 초	    128 MB	    1424436	578954	394985	    39.584%
+
+문제
+Hello World!를 출력하시오.
+
+입력
+없음
+
+출력
+Hello World!를 출력하시오.
+* */
 public class B01_2557_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
---
+++ src/B01_InputOutputAndArithmethic/README.md
@@ -0,0 +1,7 @@
+# 입출력과 사칙연산
+
+- psvm: public static void main(String[] args) {}
+- sout: System.out.println();
+
+> ***참고*** <br>
+> 자바에서는 패키지 이름과 클래스 이름이 숫자로 시작할 수 없음. 
