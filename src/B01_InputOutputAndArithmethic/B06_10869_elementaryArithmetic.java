b9763cd034e268d18194e2976da5b0e2cb657de3
hyew0 <lhy3899@gmail.com>
2026-01-12 17:36:42 +0900

    10869 elementary arithmetic

---
+++ src/B01_InputOutputAndArithmethic/B06_10869_elementaryArithmetic.java
@@ -0,0 +1,34 @@
+package B01_InputOutputAndArithmethic;
+
+
+import java.util.Scanner;
+
+/*
+시간 제한	메모리 제한
+1 초	    256 MB
+
+문제
+두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오.
+
+입력
+두 자연수 A와 B가 주어진다. (1 ≤ A, B ≤ 10,000)
+
+출력
+첫째 줄에 A+B, 둘째 줄에 A-B, 셋째 줄에 A*B, 넷째 줄에 A/B, 다섯째 줄에 A%B를 출력한다.
+* */
+public class B06_10869_elementaryArithmetic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a,b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
