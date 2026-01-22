392db194fdbc1d4426d636e823bcd89646a343fd
hyew0 <lhy3899@gmail.com>
2026-01-22 15:28:52 +0900

    10171 cat

---
+++ src/B01_InputOutputAndArithmethic/B12_10171.java
@@ -0,0 +1,10 @@
+package B01_InputOutputAndArithmethic;
+
+public class B12_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
