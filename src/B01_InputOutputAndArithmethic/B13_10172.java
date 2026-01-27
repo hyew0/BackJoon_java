2f3ff974cb56a94db9f46787b8681b22129225da
hyew0 <lhy3899@gmail.com>
2026-01-27 15:43:48 +0900

    10172 dog

---
+++ src/B01_InputOutputAndArithmethic/B13_10172.java
@@ -0,0 +1,32 @@
+package B01_InputOutputAndArithmethic;
+
+/*
+개
+시간 제한	메모리 제한
+1 초	    256 MB
+
+문제
+아래 예제와 같이 개를 출력하시오.
+
+입력
+없음.
+
+출력
+개를 출력한다.
+
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+
+* */
+public class B13_10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
