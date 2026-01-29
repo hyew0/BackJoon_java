73a83ea47e1f93506ece75dc427178941501fb97
hyew0 <lhy3899@gmail.com>
2026-01-29 21:40:42 +0900

    1330 conditional statement start

---
+++ src/B02_ConditionalStatement/B01_1330.java
@@ -0,0 +1,43 @@
+package B02_ConditionalStatement;
+
+
+import java.util.Scanner;
+
+/*
+두 수 비교하기
+시간 제한	메모리 제한
+1 초	    512 MB
+
+문제
+두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다.
+
+출력
+첫째 줄에 다음 세 가지 중 하나를 출력한다.
+
+A가 B보다 큰 경우에는 '>'를 출력한다.
+A가 B보다 작은 경우에는 '<'를 출력한다.
+A와 B가 같은 경우에는 '=='를 출력한다.
+
+제한
+-10,000 ≤ A, B ≤ 10,000
+* */
+public class B01_1330 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int A,B;
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        if (A == B) {
+            System.out.println("==");
+        } else if (A > B) {
+            System.out.println(">");
+        } else {
+            System.out.println("<");
+        }
+    }
+}
