6530a0d2ff1de04998dec5fc5b63216b6610f634
hyew0 <lhy3899@gmail.com>
2026-02-25 09:50:50 +0900

    10950

---
+++ src/B03_LoopStatement/B02_10950.java
@@ -0,0 +1,48 @@
+package B03_LoopStatement;
+
+import java.util.Scanner;
+
+/*
+A+B - 3
+시간 제한	메모리 제한
+1 초	    256 MB
+
+문제
+두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+각 테스트 케이스마다 A+B를 출력한다.
+
+예제 입력 1
+5
+1 1
+2 3
+3 4
+9 8
+5 2
+예제 출력 1
+2
+5
+7
+17
+7
+* */
+public class B02_10950 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+
+        for (int i = 0; i < count; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(a + b);
+        }
+    }
+}
